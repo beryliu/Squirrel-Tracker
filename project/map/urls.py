d48e7abe3a3be001441b758197e473efe248b53e
Zhanbo Peng <zp2226@columbia.edu>
2020-10-17 23:06:14 +0000

    part cahnge to views&urls

--- project/map/urls.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-from . import views
-urlpatterns = [
-    path('', views.map, name='map'),
-]
--- project/sightings/views.py
+++ project/sightings/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Squirrel
 
 # Create your views here.
+
+
+def map(request):
+    Squirres = Squirres.object.all()[:100] 
+    context = {
+            "Squirres": Squirres
+            }
+    return render(request, 'map/map.html', context) 
